69b5c35006d2929180473825e60aa82d6d9b5627
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-07 05:50:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
-  count = 0
   new_array = []
-  while while count < array.length do 
-    new_array.push(array[count].sqrt)
-   count += 1
+  array.length.times do |index| do 
+    new_array.push(array[index].sqrt())
   end 
+  new_array
 end
