ebcffe4694f040dfe4465f568caaeed61e38b8fc
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-07 06:02:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.length.times do |index|  
-    new_array.push(array[index]Math.sqrt(num))
+    new_array.push(array[index]** 2)
   end 
   new_array
 end
